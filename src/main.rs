c324a9568add59bbc95b11f3b2f22fc8e3e8e56d
Maurits "Mousetail" van Riezen <mousetail@mousetail.nl>
2023-09-12 15:06:04 +0200

    it runs

--- src/main.rs
+++ src/main.rs
@@ -38,16 +38,6 @@ struct OutgoingEdgeDefinition {
     direction: Vec<RelativeDirection>,
 }
 
-struct ExternalIncomingEdgeDefinition {
-    direction: AbsoluteDirection,
-    half: Vec<RelativeDirection>,
-}
-
-struct InternalIncomingEdgeDefinition {
-    tile: Tile,
-    direction: AbsoluteDirection,
-}
-
 #[allow(unused)]
 fn get_internal_edge_definition(
     tile: Tile,
@@ -148,6 +138,7 @@ fn get_internal_edge_definition(
     }
 }
 
+#[allow(unused)]
 fn get_external_edge_definition(
     tile: Tile,
     direction: AbsoluteDirection,
@@ -247,7 +238,7 @@ fn get_external_edge_definition(
                 direction: vec![RelativeDirection::Left],
             },
 
-            // East
+            // South
             (AbsoluteDirection::South, [RelativeDirection::Left]) => OutgoingEdgeDefinition {
                 edge_type: EdgeDefinitionType::Outside(AbsoluteDirection::East),
                 direction: vec![RelativeDirection::Right],
@@ -256,6 +247,17 @@ fn get_external_edge_definition(
                 edge_type: EdgeDefinitionType::Outside(AbsoluteDirection::East),
                 direction: vec![RelativeDirection::Left, RelativeDirection::Right],
             },
+
+            // West
+            (AbsoluteDirection::West, [RelativeDirection::Left]) => OutgoingEdgeDefinition {
+                edge_type: EdgeDefinitionType::Inside(Tile::E, AbsoluteDirection::East),
+                direction: vec![],
+            },
+            (AbsoluteDirection::West, [RelativeDirection::Right]) => OutgoingEdgeDefinition {
+                edge_type: EdgeDefinitionType::Inside(Tile::E, AbsoluteDirection::South),
+                direction: vec![],
+            },
+            //
             _ => {
                 panic!("Bad input for direction {direction:?} and side {side:?} and tile {tile:?}")
             }
@@ -264,17 +266,63 @@ fn get_external_edge_definition(
     }
 }
 
-fn main() {
-    let external_edge_definitions = vec![(
-        ExternalIncomingEdgeDefinition {
-            direction: AbsoluteDirection::West,
-            half: vec![],
-        },
-        OutgoingEdgeDefinition {
-            edge_type: EdgeDefinitionType::Inside(Tile::A, AbsoluteDirection::North),
-            direction: vec![],
-        },
-    )];
+#[derive(Clone, Debug, PartialEq)]
+struct TileReference(Vec<Tile>);
 
-    println!("Hello, world!");
+impl TileReference {
+    fn get_at(&self, index: usize) -> Tile {
+        if index < self.0.len() {
+            return self.0[index];
+        }
+        return TILE_PATTERN[index % TILE_PATTERN.len()];
+    }
+
+    fn set_at(&mut self, index: usize, tile: Tile) {
+        while index >= self.0.len() {
+            self.0.push(TILE_PATTERN[self.0.len() % TILE_PATTERN.len()]);
+        }
+
+        self.0[index] = tile;
+
+        while self.0[self.0.len() - 1] == TILE_PATTERN[(self.0.len() - 1) % TILE_PATTERN.len()] {
+            self.0.pop();
+        }
+    }
+
+    fn go(&self, direction: AbsoluteDirection) -> (Self, AbsoluteDirection) {
+        let mut copy = self.clone();
+
+        let mut definition = get_internal_edge_definition(self.get_at(0), direction);
+
+        let mut sides: Vec<Vec<RelativeDirection>> = vec![];
+        let mut index = 0;
+
+        loop {
+            match definition.edge_type {
+                EdgeDefinitionType::Inside(tile, direction) => {
+                    copy.set_at(index, tile);
+                    if index == 0 {
+                        return (copy, direction);
+                    } else {
+                        index -= 1;
+                        definition =
+                            get_external_edge_definition(tile, direction, sides.pop().unwrap())
+                    }
+                }
+                EdgeDefinitionType::Outside(direction) => {
+                    sides.push(definition.direction.iter().map(|i| i.invert()).collect());
+                    index += 1;
+                    definition = get_internal_edge_definition(copy.get_at(index), direction)
+                }
+            }
+        }
+    }
+}
+
+const TILE_PATTERN: [Tile; 5] = [Tile::A, Tile::B, Tile::D, Tile::E, Tile::C];
+
+fn main() {
+    let reference = TileReference(vec![]);
+
+    println!("{:?}", reference.go(AbsoluteDirection::East));
 }
