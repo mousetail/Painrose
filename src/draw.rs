85db3f48374e2b95cce33ea7c1a77298747f02e6
Maurits "Mousetail" van Riezen <mousetail@mousetail.nl>
2023-09-13 15:54:01 +0200

    drawing doesn't actually work but at least it looks cool

--- src/draw.rs
+++ src/draw.rs
@@ -3,104 +3,188 @@ use crate::penrose::{Tile, TileReference};
 use glam::f32::*;
 use std::{f32::consts, io::Write};
 
-const SCALING_FACTOR: f32 = 1.0 / 1.618033988;
+const SCALING_FACTOR: f32 = 1.618033988;
+const SCALING_FACTOR_INVERSE: f32 = 1.0 / SCALING_FACTOR;
+
+#[derive(Copy, Clone, Debug)]
+struct ShapeInfo {
+    width: f32,
+    height: f32,
+    side_length: f32,
+    bottom_angle: f32,
+    side_angle: f32,
+}
+
+impl ShapeInfo {
+    fn new(angle: f32) -> ShapeInfo {
+        let angle_radians = angle.to_radians();
+
+        let width = (angle_radians * 0.5).cos() * SCALING_FACTOR_INVERSE * 2.0;
+        let height = (angle_radians * 0.5).sin() * SCALING_FACTOR_INVERSE * 2.0;
+
+        ShapeInfo {
+            width,
+            height,
+            side_length: SCALING_FACTOR_INVERSE,
+            side_angle: angle_radians,
+            bottom_angle: consts::PI - angle_radians,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+
+struct AllShapeInfos {
+    thin_rhomb: ShapeInfo,
+    thick_rhomb: ShapeInfo,
+}
+
+impl AllShapeInfos {
+    fn new() -> AllShapeInfos {
+        AllShapeInfos {
+            thin_rhomb: ShapeInfo::new(36.0),
+            thick_rhomb: ShapeInfo::new(108.0),
+        }
+    }
+}
+
+const MIN_ITERATIONS: usize = 0;
 
 pub fn draw_svg(coords: Vec<TileReference>) {
+    let colors = [
+        "red", "green", "blue", "cyan", "magenta", "orange", "purple", "grey",
+    ];
+    let shape_info = AllShapeInfos::new();
+    println!("{shape_info:?}");
+
     let mut file = std::fs::File::create("Tiles.svg").unwrap();
 
-    file.write_all(b"<svg version=\"1.1\" height=\"106\" width=\"106\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"-5 -5 10 10\">\n").unwrap();
+    file.write_all(b"<svg version=\"1.1\" height=\"512\" width=\"512\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"-4 -4 8 8\">\n").unwrap();
 
-    for coord in coords {
-        writeln!(
-            file,
-            "<path fill=\"none\" stroke=\"black\" stroke-width=\"0.1\" d=\"M {} Z\"/>",
-            &draw(&coord)
-                .into_iter()
-                .map(|c| format!("L {} {} ", c.0, c.1))
-                .collect::<String>()[1..]
-        )
-        .unwrap();
+    for (index, coord) in coords.into_iter().enumerate() {
+        for j in [2] {
+            writeln!(
+                file,
+                "<path fill=\"none\" stroke=\"{}\" stroke-width=\"0.025\" d=\"M {} Z\" alt=\"{coord:?}\"/>",
+                if j==0{colors[index%colors.len()]} else { ["#00000044", "#22882244"][index % 2] },
+                &draw(&coord, j, shape_info)
+                    .into_iter()
+                    .map(|c| format!("L {} {} ", c.0 , c.1 ))
+                    .collect::<String>()[1..]
+            )
+            .unwrap();
+        }
     }
+
+    writeln!(
+        file,
+        "<circle cx=\"{}\" cy=\"{}\" r=\"0.05\"/>",
+        shape_info.thick_rhomb.width / 2.0,
+        shape_info.thick_rhomb.height / 2.0
+    )
+    .unwrap();
 
     file.write_all(b"</svg>").unwrap();
 }
 
-fn draw(coordinate: &TileReference) -> Vec<(f32, f32)> {
+fn draw(coordinate: &TileReference, offset: usize, shape_info: AllShapeInfos) -> Vec<(f32, f32)> {
     let mut matrix = glam::f32::Mat4::IDENTITY;
     let reference = TileReference(vec![]);
-    for index in (0..coordinate.0.len()) {
-        let (mut position, mut angle) = get_offset_and_rotation(coordinate.get_at(index));
+    for index in offset..coordinate.0.len().max(MIN_ITERATIONS) {
+        matrix = get_matrix(coordinate, index, shape_info) * matrix;
+    }
+    println!("{matrix:?}");
 
-        if index == coordinate.0.len() - 1 {
-            let (default_position, default_angle) =
-                get_offset_and_rotation(reference.get_at(index));
-            position -= default_position;
-            angle -= default_angle;
-        }
-
-        println!("{position:?} {angle:?}");
-
-        matrix = Mat4::from_translation(position)
-            * Mat4::from_quat(Quat::from_rotation_z(angle))
-            * matrix;
+    for i in (0..coordinate.0.len().max(MIN_ITERATIONS)).rev() {
+        matrix = get_matrix(&reference, i, shape_info).inverse() * matrix;
     }
 
-    let coordinates = match coordinate.get_at(0) {
-        Tile::A | Tile::C | Tile::E => vec![
-            Vec3::new(-1.0, -1.0, 0.0),
-            Vec3::new(-1.0, 1.0, 0.0),
-            Vec3::new(1.0, 1.0, 0.0),
-            Vec3::new(1.0, -1.0, 0.0),
-        ],
-        Tile::D | Tile::B => vec![
-            Vec3::new(-1.0, -1.5, 0.0),
-            Vec3::new(1.0, -1.5, 0.0),
-            Vec3::new(1.0, 1.5, 0.0),
-            Vec3::new(-1.0, 1.5, 0.0),
-        ],
+    let rhomb = match coordinate.get_at(0) {
+        Tile::A | Tile::C | Tile::E => shape_info.thick_rhomb,
+        Tile::D | Tile::B => shape_info.thin_rhomb,
     };
+
+    let coordinates = vec![
+        Vec3::new(0.0, rhomb.height / 2.0, 0.0),
+        Vec3::new(-rhomb.width / 2.0, 0.0, 0.0),
+        Vec3::new(0.0, -rhomb.height / 2.0, 0.0),
+        Vec3::new(rhomb.width / 2.0, 0.0, 0.0),
+        Vec3::new(0.0, rhomb.height / 2.0, 0.0),
+        Vec3::new(0.0, 0.0, 0.0),
+    ];
 
     return coordinates
         .into_iter()
-        .map(|i| matrix.transform_point3(i))
+        .map(|i| matrix.transform_point3(i * SCALING_FACTOR.powi(offset as i32)))
         .map(|i| (i.x, i.y))
         .collect();
 }
 
-fn get_offset_and_rotation(tile: Tile) -> (Vec3, f32) {
-    let thin_big_angle: f32 = (114.0f32).to_radians();
-    let thin_small_angle: f32 = (36.0f32).to_radians();
+fn get_matrix(coordinate: &TileReference, index: usize, shape_info: AllShapeInfos) -> Affine3A {
+    let (position, angle) = get_offset_and_rotation(coordinate.get_at(index), shape_info);
 
-    let thick_big_angle: f32 = (108.0f32).to_radians();
-    let thick_small_angle: f32 = (72.0f32).to_radians();
+    Affine3A::from_translation(position * SCALING_FACTOR.powi(index as i32 + 1))
+        * Affine3A::from_quat(Quat::from_rotation_z(angle))
+}
 
-    let thick_side_length = SCALING_FACTOR;
-    let thick_width = (thick_side_length * thick_side_length - 0.5 * 0.5).sqrt();
-
-    let thin_width = SCALING_FACTOR - SCALING_FACTOR * SCALING_FACTOR;
-    let thin_height = (SCALING_FACTOR.powf(2.0) - thin_width.powf(2.0)).sqrt();
-    println!("{thin_big_angle} {thin_small_angle} {thick_big_angle} {thick_small_angle} {thick_side_length} {thick_width} {thin_width} {thin_height}");
-
+fn get_offset_and_rotation(tile: Tile, shape_info: AllShapeInfos) -> (Vec3, f32) {
     match tile {
-        Tile::A => (Vec3::new(0.0, 1.0 - SCALING_FACTOR, 0.0), 0.0),
+        Tile::A => (
+            Vec3::new(
+                0.0,
+                (shape_info.thick_rhomb.height - SCALING_FACTOR_INVERSE) * 0.5,
+                0.0,
+            ),
+            consts::PI,
+        ),
         Tile::B => (
             Vec3::new(
-                thick_width * thick_side_length * SCALING_FACTOR * SCALING_FACTOR / thick_width,
-                0.5 * thick_side_length * SCALING_FACTOR * SCALING_FACTOR / thick_width,
+                (-shape_info.thick_rhomb.bottom_angle / 2.0 + consts::FRAC_PI_2).sin()
+                    * shape_info.thin_rhomb.width
+                    * SCALING_FACTOR_INVERSE
+                    / 2.0,
+                (-shape_info.thick_rhomb.bottom_angle / 2.0 + consts::FRAC_PI_2).cos()
+                    * shape_info.thin_rhomb.width
+                    * SCALING_FACTOR_INVERSE
+                    / 2.0
+                    + 0.5
+                    - SCALING_FACTOR_INVERSE,
                 0.0,
             ),
-            consts::FRAC_PI_2 - thin_small_angle / 2.0,
+            -shape_info.thick_rhomb.side_angle / 2.0 + consts::FRAC_PI_2,
         ),
-        Tile::C => (Vec3::new(thick_width / 2.0, -0.5 / 2.0, 0.0), 0.0),
-        Tile::D => (
+        Tile::C => (
             Vec3::new(
-                -thin_height * thin_height / SCALING_FACTOR / thick_side_length,
-                -thin_width * thin_height / SCALING_FACTOR / thick_side_length,
+                shape_info.thick_rhomb.width / 4.0,
+                -shape_info.thick_rhomb.height / 4.0,
                 0.0,
             ),
-            0.0,
+            -shape_info.thick_rhomb.bottom_angle / 2.0 + consts::PI,
         ),
-        Tile::E => (Vec3::new(-thin_height * 0.5, -thin_width * 0.5, 0.0), 0.0),
+        Tile::D => {
+            let scaling_factor = shape_info.thin_rhomb.height / 2.0;
+            assert_eq!(
+                shape_info.thin_rhomb.height * SCALING_FACTOR_INVERSE
+                    + SCALING_FACTOR_INVERSE * SCALING_FACTOR_INVERSE,
+                SCALING_FACTOR_INVERSE
+            );
+            (
+                Vec3::new(
+                    -shape_info.thin_rhomb.width * 0.5 * scaling_factor,
+                    shape_info.thin_rhomb.height * 0.5 * (1.0 - scaling_factor),
+                    0.0,
+                ),
+                shape_info.thin_rhomb.side_angle / 2.0 + consts::FRAC_PI_2,
+            )
+        }
+        Tile::E => (
+            Vec3::new(
+                -shape_info.thin_rhomb.width * 0.25,
+                -shape_info.thin_rhomb.height * 0.25,
+                0.0,
+            ),
+            shape_info.thin_rhomb.bottom_angle / 2.0 + consts::PI,
+        ),
         _ => panic!("Invalid tile"),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,18 @@ mod penrose;
 
 fn main() {
     draw::draw_svg(vec![
-        penrose::TileReference(vec![]),
         penrose::TileReference(vec![penrose::Tile::A]),
-        penrose::TileReference(vec![penrose::Tile::C]),
-        penrose::TileReference(vec![penrose::Tile::C, penrose::Tile::C]),
+        penrose::TileReference(vec![penrose::Tile::B]),
+        penrose::TileReference(vec![]),
+        penrose::TileReference(vec![penrose::Tile::D, penrose::Tile::D]),
+        penrose::TileReference(vec![penrose::Tile::E, penrose::Tile::D]),
+        penrose::TileReference(vec![penrose::Tile::A, penrose::Tile::A, penrose::Tile::A]),
+        penrose::TileReference(vec![penrose::Tile::B, penrose::Tile::A, penrose::Tile::A]),
+        penrose::TileReference(vec![penrose::Tile::C, penrose::Tile::A, penrose::Tile::A]),
+        penrose::TileReference(vec![penrose::Tile::A, penrose::Tile::C, penrose::Tile::A]),
+        penrose::TileReference(vec![penrose::Tile::B, penrose::Tile::C, penrose::Tile::A]),
+        penrose::TileReference(vec![penrose::Tile::C, penrose::Tile::C, penrose::Tile::A]),
+        penrose::TileReference(vec![penrose::Tile::D, penrose::Tile::B, penrose::Tile::A]),
+        penrose::TileReference(vec![penrose::Tile::E, penrose::Tile::B, penrose::Tile::A]),
     ])
 }
